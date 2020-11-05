6cb17c15204795dc15a2940d03016090d94f6cdf
Miguel  Arreola <marreola4619@gmail.com>
2020-11-05 17:37:27 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,9 @@ first_number = 3
 second_number = 7
 
 third_number = first_number + second_number
+
+fourth_number = first_number - second_number
+
+fifth_number = first_number * second_number
+
+sixth_number = first_number / second_number
