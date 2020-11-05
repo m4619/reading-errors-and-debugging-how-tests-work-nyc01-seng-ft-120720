f617db734796fa772ad1f095a58cee54dae7e1e7
Miguel  Arreola <marreola4619@gmail.com>
2020-11-05 17:39:41 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,8 +5,8 @@ second_number = 7
 
 sum = first_number + second_number
 
-fourth_number = first_number - second_number
+difference = first_number - second_number
 
-fifth_number = first_number * second_number
+product = first_number * second_number
 
-sixth_number = first_number / second_number
+quotient = first_number / second_number
